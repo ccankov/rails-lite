88008c423b62509a47b78bd36cce31c876124a2d
Chris Cankov <cankov.chris@gmail.com>
2017-04-25 18:01:54 -0700

    Begin implementation of the ShowExceptions middleware to better handle internal server errors

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,25 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      @app.call(env)
+    rescue StandardError => e
+      Rack::Response.new(render_exception(e),
+                         500,
+                         {'Content-Type' => 'text/html'})
+    end
   end
 
   private
 
   def render_exception(e)
-  end
 
+  end
 end
